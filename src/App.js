a961e59ee0c88236914d6d3a52c98c9c9ada8d41
cse498 <cse498@cse498-desk-64.cse.msu.edu>
2023-09-19 19:22:12 -0400

    Added home page body component and css class for it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar'
 import Footer from './components/Footer';
+import HomeBodyIntro from './components/HomeBodyIntro';
 
 function App() {
   return (
     <div id="site-content">
       <Navbar />
-      <h1 className="hello">Hello, World!</h1>
+      <HomeBodyIntro />
       <Footer />
     </div>
   );
---
+++ src/components/HomeBodyIntro.js
@@ -0,0 +1,14 @@
+function HomeBodyIntro() {
+    return (
+      <homebodyintro className="homebodyintro">
+        <h1>Climate Solutions - Optimized</h1>
+        <h3> 
+            We provide companies and agencies with the artificial intelligence tools 
+            necessary for completing their sustainability projects in the most efficient 
+            and optimal way possible.
+        </h3>
+      </homebodyintro>
+    )
+  }
+  
+  export default HomeBodyIntro
