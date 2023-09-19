27cc7f5bc8fced5bf394472e29c4ed0fd1f6c104
jacksonholscher <holsche2@msu.edu>
2023-09-19 19:18:40 -0400

    Cleaned up navbar and added comments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,16 @@ import menuBar from "./../assets/menu-bar.png"
 
 function Navbar() {
 
+  // useState hook to manage the dropdown state
   const [down, setDown] = useState(false);
 
+  /**
+   * Hamburger icon functional component
+   * 
+   * @param {boolean} props the current state of the dropdown menu
+   * 
+   * @returns the clickable hamburger icon image as an img tag
+   */
   function Hamburger(props) {
     return (
       <img 
@@ -18,6 +26,8 @@ function Navbar() {
     )
   }
 
+  // Check the current state of the dropdown menu,
+  // return the according navbar elements.
   if(down) {
     return (
       <>
@@ -39,54 +49,16 @@ function Navbar() {
       </nav>
     )
   }
-
-  // return (
-  //   <nav className="navbar">
-      
-  //     <a href="/">
-  //       <img 
-  //         src={logoImage}
-  //         alt="Anthropocene Institute Logo"
-  //         loading="lazy"
-  //         id="anth-logo"
-  //      />
-  //     </a>
-
-  //     {/* Hamburger dropdown menu for smaller screen sizes */}
-  //     <div className="dropdown-wrap">
-        
-  //       <img 
-  //         src={menuBar}
-  //         alt="Menu bar hamburger icon"
-  //         loading="lazy"
-  //         id="menubar"
-  //         onClick={() => setDown(!down)}
-  //       />
-
-  //       <ul className="navbar-dropdown">
-  //         <Navlink path="/"> Home </Navlink>
-  //         <Navlink path="/about"> About </Navlink>
-  //         <Navlink path="/tools"> Tools </Navlink>
-  //         <Navlink path="/faq"> FAQ </Navlink>
-  //         <Navlink path="/data"> Data </Navlink>
-  //         <Navlink path="/articles"> Articles </Navlink>
-  //       </ul>
-  //     </div>
-      
-      
-  //     {/* Regular nav menu for larger screen sizes */}
-  //     <ul className="navbar-nav">
-  //       <Navlink path="/"> Home </Navlink>
-  //       <Navlink path="/about"> About </Navlink>
-  //       <Navlink path="/tools"> Tools </Navlink>
-  //       <Navlink path="/faq"> FAQ </Navlink>
-  //       <Navlink path="/data"> Data </Navlink>
-  //       <Navlink path="/articles"> Articles </Navlink>
-  //     </ul>
-  //   </nav>
-  // );
 }
 
+/**
+ * Navbar link functional component
+ * 
+ * @param {String} props Contains the routing path and the link display text
+ * in props.path and props.children, repsectively.
+ * 
+ * @returns The clickable navbar link as a list item tag
+ */
 function Navlink(props) {
   return (
     <li className="navbar-item">
@@ -97,6 +69,11 @@ function Navlink(props) {
   );
 }
 
+/**
+ * The Anthropocene Institute logo
+ * 
+ * @returns AI logo as a clickable link to the homepage
+ */
 function LogoImage() {
   return (
     <a href="/">
@@ -110,6 +87,15 @@ function LogoImage() {
   )
 }
 
+/**
+ * Dropdown Navbar Menu
+ * 
+ * While this is very similar to the regular navbar menu, I believe
+ * it makes sense to have a separate component for styling purposes
+ * 
+ * @returns Dropdown menu as an unordered list of Navlinks,
+ * wrapped in a div tag for styling
+ */
 function DropdownMenu() {
   return (
     <div className="dropdown-wrap">
@@ -125,6 +111,10 @@ function DropdownMenu() {
   )
 }
 
+/**
+ * Standard navbar menu for larger screens
+ * @returns Navbar menu as an unordered list of Navlinks
+ */
 function Menu() {
   return (
     <ul className="navbar-nav">
