bf47f9077b424d9537f46f030a86330fe647292a
jacksonholscher <holsche2@msu.edu>
2023-09-19 19:08:11 -0400

    Responsive nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,90 @@
+import { useState } from "react";
 import logoImage from "./../assets/logo.png"
 import menuBar from "./../assets/menu-bar.png"
 
 function Navbar() {
-  return (
-    <nav className="navbar">
-      
-      <a href="/">
-        <img 
-          src={logoImage}
-          alt="Anthropocene Institute Logo"
-          loading="lazy"
-          id="anth-logo"
-       />
-      </a>
 
-      {/* Hamburger dropdown menu for smaller screen sizes */}
-      <div className="dropdown-wrap">
+  const [down, setDown] = useState(false);
+
+  function Hamburger(props) {
+    return (
+      <img 
+        src={menuBar}
+        alt="Menu bar hamburger icon"
+        loading="lazy"
+        id="menubar"
+        onClick={() => setDown(!props.down)}
+      />
+    )
+  }
+
+  if(down) {
+    return (
+      <>
+        <nav className="navbar">
+          <LogoImage />
+          <Hamburger down={down} />
+          <Menu />
+        </nav>
+        <DropdownMenu />
+      </>
+      
+    )
+  } else {
+    return (
+      <nav className="navbar">
+        <LogoImage />
+        <Hamburger down={down} />
+        <Menu />
+      </nav>
+    )
+  }
+
+  // return (
+  //   <nav className="navbar">
+      
+  //     <a href="/">
+  //       <img 
+  //         src={logoImage}
+  //         alt="Anthropocene Institute Logo"
+  //         loading="lazy"
+  //         id="anth-logo"
+  //      />
+  //     </a>
+
+  //     {/* Hamburger dropdown menu for smaller screen sizes */}
+  //     <div className="dropdown-wrap">
         
-        <img 
-          src={menuBar}
-          alt="Menu bar hamburger icon"
-          loading="lazy"
-          id="menubar"
-        />
+  //       <img 
+  //         src={menuBar}
+  //         alt="Menu bar hamburger icon"
+  //         loading="lazy"
+  //         id="menubar"
+  //         onClick={() => setDown(!down)}
+  //       />
 
-        <ul className="navbar-dropdown">
-          <Navlink path="/"> Home </Navlink>
-          <Navlink path="/about"> About </Navlink>
-          <Navlink path="/tools"> Tools </Navlink>
-          <Navlink path="/faq"> FAQ </Navlink>
-          <Navlink path="/data"> Data </Navlink>
-          <Navlink path="/articles"> Articles </Navlink>
-        </ul>
-      </div>
+  //       <ul className="navbar-dropdown">
+  //         <Navlink path="/"> Home </Navlink>
+  //         <Navlink path="/about"> About </Navlink>
+  //         <Navlink path="/tools"> Tools </Navlink>
+  //         <Navlink path="/faq"> FAQ </Navlink>
+  //         <Navlink path="/data"> Data </Navlink>
+  //         <Navlink path="/articles"> Articles </Navlink>
+  //       </ul>
+  //     </div>
       
       
-      {/* Regular nav menu for larger screen sizes */}
-      <ul className="navbar-nav">
-        <Navlink path="/"> Home </Navlink>
-        <Navlink path="/about"> About </Navlink>
-        <Navlink path="/tools"> Tools </Navlink>
-        <Navlink path="/faq"> FAQ </Navlink>
-        <Navlink path="/data"> Data </Navlink>
-        <Navlink path="/articles"> Articles </Navlink>
-      </ul>
-    </nav>
-  );
+  //     {/* Regular nav menu for larger screen sizes */}
+  //     <ul className="navbar-nav">
+  //       <Navlink path="/"> Home </Navlink>
+  //       <Navlink path="/about"> About </Navlink>
+  //       <Navlink path="/tools"> Tools </Navlink>
+  //       <Navlink path="/faq"> FAQ </Navlink>
+  //       <Navlink path="/data"> Data </Navlink>
+  //       <Navlink path="/articles"> Articles </Navlink>
+  //     </ul>
+  //   </nav>
+  // );
 }
 
 function Navlink(props) {
@@ -56,6 +95,47 @@ function Navlink(props) {
       </a>
     </li>
   );
+}
+
+function LogoImage() {
+  return (
+    <a href="/">
+      <img 
+        src={logoImage}
+        alt="Anthropocene Institute Logo"
+        loading="lazy"
+        id="anth-logo"
+      />
+    </a>
+  )
+}
+
+function DropdownMenu() {
+  return (
+    <div className="dropdown-wrap">
+      <ul className="navbar-dropdown">
+        <Navlink path="/"> Home </Navlink>
+        <Navlink path="/about"> About </Navlink>
+        <Navlink path="/tools"> Tools </Navlink>
+        <Navlink path="/faq"> FAQ </Navlink>
+        <Navlink path="/data"> Data </Navlink>
+        <Navlink path="/articles"> Articles </Navlink>
+      </ul>
+    </div>
+  )
+}
+
+function Menu() {
+  return (
+    <ul className="navbar-nav">
+      <Navlink path="/"> Home </Navlink>
+      <Navlink path="/about"> About </Navlink>
+      <Navlink path="/tools"> Tools </Navlink>
+      <Navlink path="/faq"> FAQ </Navlink>
+      <Navlink path="/data"> Data </Navlink>
+      <Navlink path="/articles"> Articles </Navlink>
+    </ul>
+  )
 }
 
 export default Navbar
