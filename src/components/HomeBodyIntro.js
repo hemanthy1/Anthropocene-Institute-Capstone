a63172c18467999ba7f2ee4ee69f904ea0ce59f5
cse498 <cse498@cse498-desk-64.cse.msu.edu>
2023-09-20 16:55:15 -0400

    Top of home page designed and implemented.

--- src/components/HomeBodyIntro.js
+++ src/components/HomeBodyIntro.js
@@ -1,14 +1,38 @@
+import environmentalBackground from "./../assets/EnvironmentalBackground.png"
+
 function HomeBodyIntro() {
     return (
-      <homebodyintro className="homebodyintro">
-        <h1>Climate Solutions - Optimized</h1>
-        <h3> 
-            We provide companies and agencies with the artificial intelligence tools 
-            necessary for completing their sustainability projects in the most efficient 
-            and optimal way possible.
-        </h3>
-      </homebodyintro>
+      <div>
+        <homebodyintro className="homebodyintro">
+          <EnvironmentalBackground/>
+
+          <h1>Climate Solutions - Optimized</h1>
+          <h3> 
+              We provide companies and agencies with the artificial intelligence tools 
+              necessary for completing their sustainability projects in the most efficient 
+              and optimal way possible.
+          </h3>
+        </homebodyintro>
+
+      </div>
     )
   }
   
+  /**
+ * The Anthropocene Institute logo
+ * 
+ * @returns AI logo as a clickable link to the homepage
+ */
+function EnvironmentalBackground() {
+  return (
+    <a href="/">
+      <img 
+        src={environmentalBackground}
+        alt="Environmental Backgronud"
+        loading="lazy"
+        id="environmental-bg"
+      />
+    </a>
+  )
+}
   export default HomeBodyIntro
