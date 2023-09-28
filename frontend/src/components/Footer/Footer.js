8c6e9bd9acb823655060b82d0253dc3f323ff31b
Nick Wang <wangnich@msu.edu>
2023-09-28 12:20:17 -0400

    Added my linkedin link

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -18,7 +18,7 @@ function Footer() {
                   <li className="title">Contributors</li>
                   <li className="link"><a href="https://www.linkedin.com/in/eden-haase-90b688212/">Jack Holscher</a></li>
                   <li className="link"><a href="https://www.linkedin.com/in/eden-haase-90b688212/">Ishita Kokil</a></li>
-                  <li className="link"><a href="https://www.linkedin.com/in/eden-haase-90b688212/">Nick Wang</a></li>
+                  <li className="link"><a href="https://www.linkedin.com/in/nicholas-wang7190/">Nick Wang</a></li>
                   <li className="link"><a href="https://www.linkedin.com/in/eden-haase-90b688212/">Edie Haase</a></li>
                   <li className="link"><a href="https://hemanthyalamanchili.com/ ">Hemanth Yalamanchili</a></li>
               </ul>
