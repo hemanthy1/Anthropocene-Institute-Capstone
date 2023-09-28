cea2206a24c27e1ff342e1d4fecedd30b7ae5771
Hemanth Yalamanchili <52936630+hemanthy1@users.noreply.github.com>
2023-09-27 20:54:27 -0400

    Refactor

--- backend/DatabaseConnector.py
+++ backend/DatabaseConnector.py
@@ -6,22 +6,23 @@ import pg8000
 import sqlalchemy
 
 
+# TODO     
+# Note: Saving credentials in environment variables is convenient, but not
+# secure - consider a more secure solution such as
+# Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+# keep secrets safe.
+INSTANCE_CONNECTION_NAME="carbon-mapp:us-east5:test-database" 
+DB_USER="testUser"
+DB_PASS="@]pzXU3(U@tB}Ss["
+DB_NAME="test-1"
+
+
 def connect_with_connector() -> sqlalchemy.engine.base.Engine:
     """
     Initializes a connection pool for a Cloud SQL instance of Postgres.
 
     Uses the Cloud SQL Python Connector package.
     """
-    # Note: Saving credentials in environment variables is convenient, but not
-    # secure - consider a more secure solution such as
-    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-    # keep secrets safe.
-
-    instance_connection_name =  "carbon-mapp:us-east5:test-database"  # e.g. 'project:region:instance'
-    db_user = "testUser"  # e.g. 'my-db-user'
-    db_pass = "@]pzXU3(U@tB}Ss["  # e.g. 'my-db-password'
-    db_name = "test-1"  # e.g. 'my-database'
-
     ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
 
     # initialize Cloud SQL Python Connector object
@@ -29,11 +30,11 @@ def connect_with_connector() -> sqlalchemy.engine.base.Engine:
 
     def getconn() -> pg8000.dbapi.Connection:
         conn: pg8000.dbapi.Connection = connector.connect(
-            instance_connection_name,
+            INSTANCE_CONNECTION_NAME,
             "pg8000",
-            user=db_user,
-            password=db_pass,
-            db=db_name,
+            user=DB_USER,
+            password=DB_PASS,
+            db=DB_NAME,
             ip_type=ip_type,
         )
         return conn
@@ -46,6 +47,8 @@ def connect_with_connector() -> sqlalchemy.engine.base.Engine:
     )
     return pool
 
+
+
 if __name__ == '__main__':  
     engine=connect_with_connector()
     meta=sqlalchemy.MetaData()
@@ -54,18 +57,18 @@ if __name__ == '__main__':
     sqlalchemy.Column('latitude', sqlalchemy.Float),
     sqlalchemy.Column('longitude', sqlalchemy.Float),
     )
+    # TO CREATE TABLE
     # Reforestation.create(engine)   
     
     with engine.connect() as connection:
+        
+        # TO INSERT INTO TABLE
         # connection.execute(Reforestation.insert(), {"land_price": 111111.0, "latitude": 599.0, "longitude": 695.0})
         # connection.execute(Reforestation.insert(), {"land_price": 101010.0, "latitude": 69.0, "longitude": 34.0})
         # connection.execute(Reforestation.insert(), {"land_price": 101010.0, "latitude": 1208.0, "longitude": 234.0})
         # connection.commit()
+        
         result = connection.execute(sqlalchemy.text("select reforestation_table.longitude from reforestation_table"))
         for row in result:
-            print("Longitude:", row.longitude)
-
-        # result = connection.execute(sqlalchemy.select([Reforestation.Land_Price]))
-        # land_prices_list = [row['land_price'] for row in result]
-        # print(land_prices_list)     
+            print("Longitude:", row.longitude) 
         
