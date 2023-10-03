35405d9d0b212ed48d543008d84248610b1f9cfe
Nick Wang <wangnich@msu.edu>
2023-10-03 18:28:47 -0400

    added about page outline/css file

--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <h1>ABOUT</h1>
+      
+      <h2>Project overview</h2>
+    
+      <h2>Motivation?</h2>
+
+      <h2>Methodology</h2>
+      <h3>Algae Blooms</h3>
+      <h3>Reforestation</h3>
+      <h3>DAC</h3>
+
+
+      <h2>Design Day Page/Talk about capstone?</h2>
+
+    </div>
   )
 }
 
