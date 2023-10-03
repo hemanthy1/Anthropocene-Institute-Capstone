50fa9992bd330e8592052aa6ed7e7bed6d86295a
Ishita Kokil <kokilish@msu.edu>
2023-10-03 14:23:53 -0400

    Michigan Reforestation Data
    Combined data for reforestation model (renamed file), Added it to the forestationdb database as a new table 'mi', test file to retrieve data and see if things got appended and work.

---
+++ backend/test_forestationDB.py
@@ -0,0 +1,28 @@
+
+#TEST if stuff was appended to forestation db and table. 
+import pandas as pd
+import sqlalchemy
+from DatabaseConnector import connect_with_connector
+
+INSTANCE_CONNECTION_NAME="carbon-mapp:us-east5:forestationdb"
+DB_USER="teamanthro"
+DB_PASS="Anthinst498"
+DB_NAME="mi-values"
+
+engine = connect_with_connector(INSTANCE_CONNECTION_NAME, DB_USER, DB_PASS, DB_NAME)
+
+def first_five():
+    with engine.connect() as connection:
+        query = "SELECT * FROM mi LIMIT 5"
+        results_df = pd.read_sql(query, connection)
+        print(results_df)
+
+def count_rows():
+    with engine.connect() as connection:
+        query = sqlalchemy.text("SELECT COUNT(*) FROM mi")
+        result = connection.execute(query).scalar()
+        print(result)
+
+if __name__ == "__main__":
+    first_five()
+    count_rows()
