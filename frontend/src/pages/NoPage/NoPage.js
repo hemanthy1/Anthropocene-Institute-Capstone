7ce131b28d1761be5968acc1c28bb598388d33ad
jacksonholscher <holsche2@msu.edu>
2023-10-09 14:40:00 -0400

    Added to and styled the error 404 page.

--- frontend/src/pages/NoPage/NoPage.js
+++ frontend/src/pages/NoPage/NoPage.js
@@ -1,6 +1,22 @@
+import "./NoPage.css"
+import logo from "../../assets/logoNormal.png"
+
 function NoPage() {
   return(
-    <h1>Oops! 404, Page Not Found!</h1>
+    <>
+      <h1 id="error-title">Oops! 404, Page Not Found!</h1>
+      <h2 id="error-message"> 
+        We apologize for the inconvenience. 
+        <a href="/" id="return-link"> Click here </a>
+        to return to the home page.
+      </h2>
+      <img
+        src={logo}
+        alt="Anthropocene Logo"
+        loading="lazy"
+        id="error-logo"
+      />
+    </>
   );
 };
 
