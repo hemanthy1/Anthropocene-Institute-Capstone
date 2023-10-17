9588290dc1d3da53fcacc7daf1260b8b2347b35d
Nick Wang <wangnich@msu.edu>
2023-10-17 17:48:53 -0400

    Created json files for output, and started formatting function

--- backend/geoJSONFormatter.py
+++ backend/geoJSONFormatter.py
@@ -1,10 +1,31 @@
 import json
-
-
+import shutil
 
 def updateReforestationDataState(stateJsonFile, stateReforestationCsv):
 
+    shutil.copyfile(stateJsonFile, "backend/geoJsonOutputs/stateData/stateReforestation.json")
 
+    new_file = "backend/geoJsonOutputs/stateData/stateReforestation.json"
+
+    # Write to new json file, NOT WORK
+    with open(new_file, 'r+') as file:
+        file_data = json.load(file)
+
+        for feature in file_data["features"]:
+            print(feature["properties"])
+            feature["properties"] = {"testing": "testing"}
+
+    file.close()
+
+    print("\n\n\n")
+    # This just is to print to see if changes were made
+    with open(new_file, 'r') as file:
+        file_data = json.load(file)
+
+        for feature in file_data["features"]:
+            print(feature["properties"])
+    
+    file.close()
     return
 
 
@@ -45,3 +66,11 @@ def updateDACDataCounty(countyJsonFile, countyDACCsv):
 
     return
 
+
+
+updateReforestationDataState("backend/states.json", "dataOutputCSVs/stateData/stateReforestationCsv.csv")
+# updateAlgaeBloomDataState("states.json", "dataOutputCSVs/stateData/stateAlgaeBloomCsv.csv")
+# updateDACDataState("states.json", "dataOutputCSVs/stateData/stateDACCsv.csv")
+# updateReforestationDataCounty("counties.json", "dataOutputCSVs/countyData/countyReforestationCsv.csv")
+# updateAlgaeBloomDataCounty("counties.json", "dataOutputCSVs/countyData/countyAlgaeBloomCsv.csv")
+# updateDACDataCounty("counties.json", "dataOutputCSVs/countyData/countyDACCsv.csv")
