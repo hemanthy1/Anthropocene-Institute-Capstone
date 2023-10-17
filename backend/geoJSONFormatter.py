18b236068058e87d3a9c98599430cda1b3da41a2
Nick Wang <wangnich@msu.edu>
2023-10-17 16:51:17 -0400

    Created functions for creating the proper GeoJSON files

---
+++ backend/geoJSONFormatter.py
@@ -0,0 +1,47 @@
+import json
+
+
+
+def updateReforestationDataState(stateJsonFile, stateReforestationCsv):
+
+
+    return
+
+
+
+def updateAlgaeBloomDataState(stateJsonFile, stateAlgaeBloomCSV):
+
+
+    return
+
+
+
+def updateDACDataState(stateJsonFile, stateDACCsv):
+
+
+    return
+
+
+
+
+
+
+def updateReforestationDataCounty(countyJsonFile, countyReforestationCsv):
+
+
+    return
+
+
+
+def updateAlgaeBloomDataCounty(countyJsonFile, countyAlgaeBloomCsv):
+
+
+    return
+
+
+
+def updateDACDataCounty(countyJsonFile, countyDACCsv):
+
+
+    return
+
