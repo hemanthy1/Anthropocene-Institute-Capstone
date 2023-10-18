64053a459c04b16c216b320415166bd979ba9447
jacksonholscher <holsche2@msu.edu>
2023-10-18 18:16:40 -0400

    First draft algo for formatting state-level reforest data

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Ishita Kokil - kokilish@msu.edu
 Nick Wang - wangnich@msu.edu
 
 ## Description
-As apart of our senior capstone project at Michigan State University, we teamed up with the [Anthropocene Institute](https://anthropoceneinstitute.com) and set out to create a machine learning model that analyzes methods of removing carbon dioxide from the Earth's atmosphere. The end goal and essence of the project is optimizing the location and cost of future projects within the field of carbon sequestration and environmental sustainability as a whole, all displayed in a user-friendly web application.
+As apart of our senior capstone project at Michigan State University, we teamed up with the [Anthropocene Institute](https://anthropoceneinstitute.com) and set out to create a machine learning model that analyzes factors related to methods of removing carbon dioxide from the Earth's atmosphere. The end goal and essence of the project is optimizing the location and cost of future projects within the field of carbon sequestration and environmental sustainability as a whole, all displayed in a user-friendly web application.
 
 ## Authors and acknowledgment
 Special thanks to our wonderful project sponsors from Anthropocene:
@@ -24,8 +24,14 @@ Melinda Chow Alankar - Director of Investment and Funding Opportunities
 
 Frank Hiroshi Ling, PhD - Chief Scientist
 
+Carl Page - President
+
+Mike Villena - Software Engineer Manager
+
+Joanna Reyes - UI/UX/Art Director
+
 ## License
 &copy; Anthropocene Institute, All Rights Reserved
 
 ## Project status
-The project is currently in the early stages of development.
+The project is currently in the early stages of beta development.
--- backend/geoJSONFormatter.py
+++ backend/geoJSONFormatter.py
@@ -1,32 +1,69 @@
 import json
 import shutil
+import pandas as pd
 
 def updateReforestationDataState(stateJsonFile, stateReforestationCsv):
 
+    # file path variables
     new_file = "backend/geoJsonOutputs/stateData/stateReforestation.json"
+    data_file = "backend/filter-reforest.csv"
 
+    # copy the boundary file into a new json file
     shutil.copyfile(stateJsonFile, new_file)
 
 
-    # Write to new json file, NOT WORK
-    with open(new_file, 'r+') as file:
+    # Open the json copyfile and store the json in a python object
+    with open(new_file, 'r') as file:
         file_data = json.load(file)
 
-        for feature in file_data["features"]:
-            print(feature["properties"])
-            feature["properties"]["TESTING"] = "testestest"
+    # Store our data in a pandas DataFrame
+    df = pd.read_csv(data_file)
 
-    with open(new_file, 'w') as file:
+    # Average the data from each county, group by state
+    state_average = df.groupby('State').mean()
+
+    # Add our data into the json object
+    for feature in file_data["features"]:
+        state = feature["properties"]["NAME"]
+
+        # Ensure we are at a state we have data for
+        if state in state_average.index:
+            feature["properties"]["population"] = state_average.loc[state, "Population"]
+            feature["properties"]["temperature"] = state_average.loc[state, "Temperature"]
+            feature["properties"]["precipitation"] = state_average.loc[state, "Precipitation"]
+            feature["properties"]["palmer"] = state_average.loc[state, "Palmer-Z"]
+            feature["properties"]["cost"] = state_average.loc[state, "percentile_rank"]
+            feature["properties"]["class"] = state_average.loc[state, "final-class"]
+            feature["properties"]["isState"] = "yes"
+        else:
+            feature["properties"]["class"] = 1  # No data available at this state
+            feature["properties"]["isState"] = "yes"
+
+    # Dump the updated json object into a new file
+    with open(new_file, "w") as file:
         json.dump(file_data, file)
 
-    print("\n\n\n")
 
-    with open(new_file, "r") as file:
-        file_data = json.load(file)
+    # Quick test
+    #
+    #
+    # with open(new_file, "r") as file:
+    #     file_data = json.load(file)
+    #     for feature in file_data["features"]:
+    #         if feature["properties"]["NAME"] in state_average.index:
+    #             print("--------------------- Updated State Data: ---------------------")
+    #             print(feature["properties"])
+    #             print("--------------------- Updated State Data ---------------------")
+    #             print()
 
-        for feature in file_data["features"]:
-            print(feature["properties"])
-
+    #     for feature in file_data["features"]:
+    #         if feature["properties"]["NAME"] not in state_average.index:
+    #             print("--------------------- Non-Updated State Data: ---------------------")
+    #             print(feature["properties"])
+    #             print("--------------------- Non-Updated State Data ---------------------")
+    #             print()
+    #
+    #
     return
 
 
