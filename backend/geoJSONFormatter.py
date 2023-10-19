9b825a08fea22420882cab529945d9c3f8dc645e
jacksonholscher <holsche2@msu.edu>
2023-10-18 20:11:44 -0400

    First draft algo for formatting county-level reforest data

--- backend/geoJSONFormatter.py
+++ backend/geoJSONFormatter.py
@@ -2,6 +2,7 @@ import json
 import shutil
 import pandas as pd
 
+'''
 def updateReforestationDataState(stateJsonFile, stateReforestationCsv):
 
     # file path variables
@@ -32,6 +33,7 @@ def updateReforestationDataState(stateJsonFile, stateReforestationCsv):
             feature["properties"]["temperature"] = state_average.loc[state, "Temperature"]
             feature["properties"]["precipitation"] = state_average.loc[state, "Precipitation"]
             feature["properties"]["palmer"] = state_average.loc[state, "Palmer-Z"]
+            feature["properties"]["land"] = state_average.loc[state, "Land-Prices"]
             feature["properties"]["cost"] = state_average.loc[state, "percentile_rank"]
             feature["properties"]["class"] = state_average.loc[state, "final-class"]
             feature["properties"]["isState"] = "yes"
@@ -55,7 +57,7 @@ def updateReforestationDataState(stateJsonFile, stateReforestationCsv):
     #             print(feature["properties"])
     #             print("--------------------- Updated State Data ---------------------")
     #             print()
-
+    #
     #     for feature in file_data["features"]:
     #         if feature["properties"]["NAME"] not in state_average.index:
     #             print("--------------------- Non-Updated State Data: ---------------------")
@@ -65,7 +67,7 @@ def updateReforestationDataState(stateJsonFile, stateReforestationCsv):
     #
     #
     return
-
+'''
 
 
 def updateAlgaeBloomDataState(stateJsonFile, stateAlgaeBloomCSV):
@@ -84,11 +86,89 @@ def updateDACDataState(stateJsonFile, stateDACCsv):
 
 
 
-
+'''
 def updateReforestationDataCounty(countyJsonFile, countyReforestationCsv):
+    # file path variables
+    new_file = "backend/geoJsonOutputs/countyData/countyReforestation.json"
+    data_file = "backend/filter-reforest.csv"
 
+    # copy the boundary file into a new json file
+    shutil.copyfile(countyJsonFile, new_file)
+
+    # open the json copyfile and store the json in a python object
+    with open(new_file, 'r', encoding='ISO-8859-1') as file:
+        file_data = json.load(file)
+
+    # store our data in a pandas DataFrame
+    df = pd.read_csv(data_file)
+
+    # specify state FIPS codes for the states we have data for
+    codes = {
+        "Georgia": 13,
+        "Illinois": 17,
+        "Indiana": 18,
+        "Michigan": 26,
+        "Minnesota": 27,
+        "Nebraska": 31,
+        "North Carolina": 37,
+        "Ohio": 39,
+        "Oklahoma": 40,
+        "South Carolina": 45,
+        "Wisconsin": 55
+    }
+    
+    
+    # Add our data into the json object
+    for feature in file_data["features"]:
+        county_name = feature["properties"].get("NAME")  # county name
+        state_code = feature["properties"].get("STATE")  # state FIPS code
+        
+        # see if this county is in our data
+        match = df.loc[(df['County'] == county_name) & (df['State'].map(codes) == int(state_code))]
+        
+        if not match.empty:
+            # found a match
+            feature["properties"]["population"] = int(match.iloc[0]["Population"])
+            feature["properties"]["temperature"] = match.iloc[0]["Temperature"]
+            feature["properties"]["precipitation"] = match.iloc[0]["Precipitation"]
+            feature["properties"]["palmer"] = match.iloc[0]["Palmer-Z"]
+            feature["properties"]["land"] = match.iloc[0]["Land-Prices"]
+            feature["properties"]["cost"] = match.iloc[0]["percentile_rank"]
+            feature["properties"]["class"] = int(match.iloc[0]["final-class"])
+            feature["properties"]["isState"] = "no"
+        else:
+            # no match
+            feature["properties"]["class"] = 1  # lowest class because no data
+            feature["properties"]["isState"] = "no"
+
+    # Dump the updated json object into a new file
+    with open(new_file, "w") as file:
+        json.dump(file_data, file)
+
+    # Quick test
+    # 
+    #
+    # with open(new_file, "r") as file:
+    #     file_data = json.load(file)
+    #     updated = 0
+    #     non = 0
+    #     for feature in file_data["features"]:
+    #         if "cost" in feature['properties']:
+    #             print("------------------ Updated County Data: ------------------")
+    #             print(feature["properties"])
+    #             print("------------------ Updated County Data  ------------------")
+    #             print()
+    #             updated += 1
+    #         else:
+    #             non += 1
+    # print(f"{updated} counties were updated out of {len(df)} counties in the data file")
+    # print(f"{non} counties were not updated. There are 3,244 counties in the USA")
+    # print(f"{updated} + {non} = {updated+non}")
+    #
+    #
 
     return
+'''
 
 
 
@@ -104,11 +184,14 @@ def updateDACDataCounty(countyJsonFile, countyDACCsv):
 
     return
 
+def main():
+    # updateReforestationDataState("backend/states.json", "dataOutputCSVs/stateData/stateReforestationCsv.csv")
+    # updateAlgaeBloomDataState("backend/states.json", "dataOutputCSVs/stateData/stateAlgaeBloomCsv.csv")
+    # updateDACDataState("backend/states.json", "dataOutputCSVs/stateData/stateDACCsv.csv")
+    # updateReforestationDataCounty("backend/counties.json", "dataOutputCSVs/countyData/countyReforestationCsv.csv")
+    # updateAlgaeBloomDataCounty("backend/counties.json", "dataOutputCSVs/countyData/countyAlgaeBloomCsv.csv")
+    # updateDACDataCounty("backend/counties.json", "dataOutputCSVs/countyData/countyDACCsv.csv")
+    pass
 
-
-updateReforestationDataState("backend/states.json", "dataOutputCSVs/stateData/stateReforestationCsv.csv")
-# updateAlgaeBloomDataState("states.json", "dataOutputCSVs/stateData/stateAlgaeBloomCsv.csv")
-# updateDACDataState("states.json", "dataOutputCSVs/stateData/stateDACCsv.csv")
-# updateReforestationDataCounty("counties.json", "dataOutputCSVs/countyData/countyReforestationCsv.csv")
-# updateAlgaeBloomDataCounty("counties.json", "dataOutputCSVs/countyData/countyAlgaeBloomCsv.csv")
-# updateDACDataCounty("counties.json", "dataOutputCSVs/countyData/countyDACCsv.csv")
+if __name__ == "__main__":
+    main()
