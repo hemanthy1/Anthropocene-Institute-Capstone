82718c056269bb577ca20e0e52e9b93b7f69065a
Edie B Haase <haaseede@msu.edu>
2023-10-25 12:33:55 -0400

    Adding dropdown menu to map

--- frontend/src/components/Heatmap/Heatmap.js
+++ frontend/src/components/Heatmap/Heatmap.js
@@ -9,138 +9,152 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 function ChoroplethMap(props) {
 
-  const mapContainer = useRef(null);
-  const [legendDisplay] = useState('block');
-  const zoomThreshold = 3;
+    const mapContainer = useRef(null);
+    const [legendDisplay] = useState('block');
+    const [dropdownDisplay] = useState('block');
+    const zoomThreshold = 3;
 
-  useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiaGFhc2VlZGUiLCJhIjoiY2xuOTNwdmVxMDM0bjJtbjJxeHczYmhkbiJ9.LVjKKnnuccvPSd4rJG3uJQ';
+    useEffect(() => {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiaGFhc2VlZGUiLCJhIjoiY2xuOTNwdmVxMDM0bjJtbjJxeHczYmhkbiJ9.LVjKKnnuccvPSd4rJG3uJQ';
 
-    const map = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/light-v11',
-      center: [-98, 38.88],
-      minZoom: 2,
-      zoom: 2.2,
-    });
-    // Add Geocoder which creates the search bar
-    const geocoder = new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl,
-    });
+        const map = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/light-v11',
+            center: [-98, 38.88],
+            minZoom: 2,
+            zoom: 2.2,
+        });
+        // Add Geocoder which creates the search bar
+        const geocoder = new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            mapboxgl: mapboxgl,
+        });
 
-    // Add the search bar to the heatmap 
-    map.addControl(geocoder);
+        // Add the search bar to the heatmap
+        map.addControl(geocoder);
 
-    map.on('load', () => {
-      map.addSource('population', {
-        type: 'vector',
-        url: 'mapbox://mapbox.660ui7x6',
-      });
+        map.on('load', () => {
+            map.addSource('population', {
+                type: 'vector',
+                url: 'mapbox://mapbox.660ui7x6',
+            });
 
-      map.addLayer({
-        id: 'state-population',
-        source: 'population',
-        'source-layer': 'state_county_population_2014_cen',
-        maxzoom: zoomThreshold,
-        type: 'fill',
-        filter: ['==', 'isState', true],
-        paint: {
-          'fill-color': [
-            'interpolate',
-            ['linear'],
-            ['get', 'population'],
-            1000,
-            props.colors.color1,
-            5000,
-            props.colors.color2,
-            10000,
-            props.colors.color3,
-            50000,
-            props.colors.color4,
-            100000,
-            props.colors.color5,
-            500000,
-            props.colors.color6,
-            1000000,
-            props.colors.color7,
-          ],
-          'fill-opacity': .85,
-        },
-      });
+            map.addLayer({
+                id: 'state-population',
+                source: 'population',
+                'source-layer': 'state_county_population_2014_cen',
+                maxzoom: zoomThreshold,
+                type: 'fill',
+                filter: ['==', 'isState', true],
+                paint: {
+                    'fill-color': [
+                        'interpolate',
+                        ['linear'],
+                        ['get', 'population'],
+                        1000,
+                        props.colors.color1,
+                        5000,
+                        props.colors.color2,
+                        10000,
+                        props.colors.color3,
+                        50000,
+                        props.colors.color4,
+                        100000,
+                        props.colors.color5,
+                        500000,
+                        props.colors.color6,
+                        1000000,
+                        props.colors.color7,
+                    ],
+                    'fill-opacity': .85,
+                },
+            });
 
-      map.addLayer({
-        id: 'county-population',
-        source: 'population',
-        'source-layer': 'state_county_population_2014_cen',
-        minzoom: zoomThreshold,
-        type: 'fill',
-        filter: ['==', 'isCounty', true],
-        paint: {
-          'fill-color': [
-            'interpolate',
-            ['linear'],
-            ['get', 'population'],
-            1000,
-            props.colors.color1,
-            5000,
-            props.colors.color2,
-            10000,
-            props.colors.color3,
-            50000,
-            props.colors.color4,
-            100000,
-            props.colors.color5,
-            500000,
-            props.colors.color6,
-            1000000,
-            props.colors.color7,
-          ],
-          'fill-opacity': .85,
-        },
-      });
-    });
+            map.addLayer({
+                id: 'county-population',
+                source: 'population',
+                'source-layer': 'state_county_population_2014_cen',
+                minzoom: zoomThreshold,
+                type: 'fill',
+                filter: ['==', 'isCounty', true],
+                paint: {
+                    'fill-color': [
+                        'interpolate',
+                        ['linear'],
+                        ['get', 'population'],
+                        1000,
+                        props.colors.color1,
+                        5000,
+                        props.colors.color2,
+                        10000,
+                        props.colors.color3,
+                        50000,
+                        props.colors.color4,
+                        100000,
+                        props.colors.color5,
+                        500000,
+                        props.colors.color6,
+                        1000000,
+                        props.colors.color7,
+                    ],
+                    'fill-opacity': .85,
+                },
+            });
+        });
 
-    map.on('click', 'county-population', (e) => {
+        map.on('click', 'county-population', (e) => {
 
-      let dropdown = document.getElementById("dropdown");
-      let title = dropdown.options[dropdown.selectedIndex].text
+            let dropdown = document.getElementById("dropdown");
+            let title = dropdown.options[dropdown.selectedIndex].text
 
-      new mapboxgl.Popup()
-      .setLngLat(e.lngLat)
-      .setHTML(title + ": " + e.features[0].properties[dropdown.value])
-      .addTo(map);
-    });
+            new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(title + ": " + e.features[0].properties[dropdown.value])
+                .addTo(map);
+        });
 
-    
-    map.on('mouseenter', 'county-population', () => {
-      map.getCanvas().style.cursor = 'pointer';
-    });
-       
-    map.on('mouseleave', 'county-population', () => {
-      map.getCanvas().style.cursor = '';
-    });
 
-  }, []);
+        map.on('mouseenter', 'county-population', () => {
+            map.getCanvas().style.cursor = 'pointer';
+        });
 
-  return (
+        map.on('mouseleave', 'county-population', () => {
+            map.getCanvas().style.cursor = '';
+        });
 
-      <div ref={mapContainer} className="map-container">
-        <div id="legend" className="legend" style={{ display: legendDisplay }}>
-        <h4>Cost Efficency</h4>
-            <h3>Most</h3>
-            <div><span className= "b723122" style={{backgroundColor: props.colors.color7}}></span></div>
-            <div><span className= "b8b4225" style={{backgroundColor: props.colors.color6}}></span></div>
-            <div><span className= "ba25626" style={{backgroundColor: props.colors.color5}}></span></div>
-            <div><span className= "bb86b25" style={{backgroundColor: props.colors.color4}}></span></div>
-            <div><span className= "bca8323" style={{backgroundColor: props.colors.color3}}></span></div>
-            <div><span className= "bda9c20" style={{backgroundColor: props.colors.color2}}></span></div>
-            <div><span className= "be6b71e" style={{backgroundColor: props.colors.color1}}></span></div>
-            <h3>Least</h3>
-      </div>
-     </div>
+    }, []);
 
-  );
+    return (
+
+        <div ref={mapContainer} className="map-container">
+
+            <div className="map-overlay top" style={{display: dropdownDisplay}}>
+                <div className="map-overlay-inner">
+                    <fieldset>
+                        <label>Chose Criteria</label>
+                        <select id="layer" name="layer">
+                            <option value="population">Population</option>
+                            <option value="landPrices">Land Prices</option>
+                            <option value="precipitation">Precipitation</option>
+                        </select>
+                    </fieldset>
+                </div>
+            </div>
+            <div id="legend" className="legend" style={{display: legendDisplay}}>
+                <h4>Cost Efficency</h4>
+                <h3>Most</h3>
+                <div><span className="b723122" style={{backgroundColor: props.colors.color7}}></span></div>
+                <div><span className="b8b4225" style={{backgroundColor: props.colors.color6}}></span></div>
+                <div><span className="ba25626" style={{backgroundColor: props.colors.color5}}></span></div>
+                <div><span className="bb86b25" style={{backgroundColor: props.colors.color4}}></span></div>
+                <div><span className="bca8323" style={{backgroundColor: props.colors.color3}}></span></div>
+                <div><span className="bda9c20" style={{backgroundColor: props.colors.color2}}></span></div>
+                <div><span className="be6b71e" style={{backgroundColor: props.colors.color1}}></span></div>
+                <h3>Least</h3>
+            </div>
+        </div>
+
+    );
 }
 
 export default ChoroplethMap;
