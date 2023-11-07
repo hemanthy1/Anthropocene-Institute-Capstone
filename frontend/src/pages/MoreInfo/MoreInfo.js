da244ab6e43ac1e69ef68d6c95764f3d1f7150f6
Edie B Haase <haaseede@msu.edu>
2023-11-06 21:34:03 -0500

    Starting to organize more info page

--- frontend/src/pages/MoreInfo/MoreInfo.js
+++ frontend/src/pages/MoreInfo/MoreInfo.js
@@ -1,57 +1,30 @@
 import React, {useState} from 'react'
 import Accordion from "../../components/Accordion/Accordion";
-import  "../../components/Accordion/Accordion.css";
+import "../../components/Accordion/Accordion.css";
+import "./MoreInfo.css"
 
 
-
+/**
+ * Holds the questions and answers of the FAQs.
+ *
+ * @returns The contents of the FAQ page
+ */
 function MoreInfo() {
 
-    const [faqs, setfaqs] = useState([
-        {
-
-            question: 'What is the Name? ',
-            answer: 'explain what the name is ',
-            open: true
-
-        },
-        {
-            question: 'What is cost efficiency and how is it calculated? ',
-            answer: 'info about how cost efficiency is calculated',
-            open: false
-        },
-        {
-            question: 'what is land prices?',
-            answer: 'info on the land prices data',
-            open: false
-
-        }
-
-    ])
-
-    /**
-     * Toggles the faq when needed. This will display the answer when clicked on
-     *
-     * @returns The faq with a new state of open or closed
-     */
-    const toggleFAQ = index => {
-        setfaqs(faqs.map((faq, i) => {
-            if (i === index) {
-                faq.open = !faq.open
-            } else {
-                faq.open = false;
-            }
-            return faq;
-        }))
-    }
-
     return (
-        <div className="faqs">
+        <div>
             <div>
-                <h1 className="faqTitle">MoreInfo</h1>
+                <h1 className="more-info-title">More Info</h1>
+                <hr className="more-info-line"/>
+                <h2 className="type-title">Kelp Farms</h2>
+                <h3 className="feature"> Land Prices</h3>
+                <h3 className="feature"> Palmer-z Index</h3>
+                <h3 className="feature"> Population</h3>
+                <h3 className="feature"> Precipitation</h3>
+                <h3 className="feature"> Temperature</h3>
             </div>
-        {faqs.map((faq,i) => (
-            <Accordion key={i} faq={faq} index={i} toggleFAQ={toggleFAQ} />
-        )) }
+
+
         </div>
     )
 }
