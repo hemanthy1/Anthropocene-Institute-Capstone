ae0833ebda22986549886e343e400c671dd99d9f
jacksonholscher <holsche2@msu.edu>
2023-11-07 17:37:53 -0500

    Kelp data to GeoJson

--- backend/geoJSONFormatter.py
+++ backend/geoJSONFormatter.py
@@ -2,7 +2,7 @@ import json
 import shutil
 import pandas as pd
 
-def updateReforestationDataState(stateJsonFile, stateReforestationCsv):
+def updateReforestationDataState(stateJsonFile):
     # file path variables
     new_file = "backend/geoJsonOutputs/stateData/stateReforestation.json"
     data_file = "backend/Reforestation/reforestation_class.csv"
@@ -42,40 +42,47 @@ def updateReforestationDataState(stateJsonFile, stateReforestationCsv):
     # Dump the updated json object into a new file
     with open(new_file, "w") as file:
         json.dump(file_data, file)
-
-
-    # Quick test
-    # with open(new_file, "r") as file:
-    #     file_data = json.load(file)
-    #     updated, non = 0, 0
-    #     print("Updated: --------------------------")
-    #     for feature in file_data["features"]:
-    #         if feature["properties"]["NAME"] in state_average.index:
-    #             print("--------------------- Updated State Data: ---------------------")
-    #             print(feature["properties"])
-    #             print("--------------------- Updated State Data ---------------------")
-    #             print()
-    #             updated += 1
-    #     for feature in file_data["features"]:
-    #         if feature["properties"]["NAME"] not in state_average.index:
-    #             print("--------------------- Non-Updated State Data: ---------------------")
-    #             print(feature["properties"])
-    #             print("--------------------- Non-Updated State Data ---------------------")
-    #             print()
-    #             non += 1
-    #     print(f"{updated} states were updated. {non} states weren't. {updated} + {non} = {updated + non}")
-    #     print("Alaska, D.C., and Puerto Rico...")
     
     return
 
 
-def updateAlgaeBloomDataState(stateJsonFile, stateAlgaeBloomCSV):
+def updateKelpFarmData():
+    # file path variables
+    new_file = "backend/geoJsonOutputs/stateData/stateKelpFarm.json"
+    data_file = "backend/Kelp/kelp_class.csv"
 
+    # Store our data in a pandas DataFrame
+    df = pd.read_csv(data_file)
+
+    # build the features data as a python list
+    features = []
+    for index, row in df.iterrows():
+        feature = {
+            "type": "Feature",
+            "properties": {
+                "depth": row["Depth"],
+                "temperature": row["Temperature"],
+                "ph": row["pH"],
+                "loc": row["percentile_rank"] 
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [row["latitude"], row["longitude"]]
+            }
+        }
+        features.append(feature)
+
+    # format the final json object
+    data = {"type": "FeatureCollection", "features": features}
+
+    # write our json object to the file
+    with open(new_file, "w") as file:
+        json.dump(data, file)        
 
     return
 
 
-def updateDACDataState(stateJsonFile, stateDACCsv):
+def updateDACDataState(stateJsonFile):
     # file path variables
     new_file = "backend/geoJsonOutputs/stateData/stateDAC.json"
     data_file = "backend/DAC/dac_class.csv"
@@ -115,34 +122,11 @@ def updateDACDataState(stateJsonFile, stateDACCsv):
     # Dump the updated json object into a new file
     with open(new_file, "w") as file:
         json.dump(file_data, file)
-
-
-    # Quick test
-    with open(new_file, "r") as file:
-        file_data = json.load(file)
-        updated, non = 0, 0
-        print("Updated: --------------------------")
-        for feature in file_data["features"]:
-            if feature["properties"]["NAME"] in state_average.index:
-                print("--------------------- Updated State Data: ---------------------")
-                print(feature["properties"])
-                print("--------------------- Updated State Data ---------------------")
-                print()
-                updated += 1
-        for feature in file_data["features"]:
-            if feature["properties"]["NAME"] not in state_average.index:
-                print("--------------------- Non-Updated State Data: ---------------------")
-                print(feature["properties"])
-                print("--------------------- Non-Updated State Data ---------------------")
-                print()
-                non += 1
-        print(f"{updated} states were updated. {non} states weren't. {updated} + {non} = {updated + non}")
-        print("Alaska, D.C., and Puerto Rico...")
     
     return
 
 
-def updateReforestationDataCounty(countyJsonFile, countyReforestationCsv):
+def updateReforestationDataCounty(countyJsonFile):
     # file path variables
     new_file = "backend/geoJsonOutputs/countyData/countyReforestation.json"
     data_file = "backend/Reforestation/reforestation_class.csv"
@@ -239,55 +223,11 @@ def updateReforestationDataCounty(countyJsonFile, countyReforestationCsv):
     # Dump the updated json object into a new file
     with open(new_file, "w") as file:
         json.dump(file_data, file)
-
-    # Quick test
-    # with open(new_file, "r") as file:
-    #     file_data = json.load(file)
-    #     updated = 0
-    #     non = 0
-    #     state_set = set()
-    #     non_list = list()
-    #     for feature in file_data["features"]:
-    #         if "cost" in feature['properties']:
-    #             updated += 1
-    #             state_set.add(feature["properties"]["STATE"])
-    #         else:
-    #             if feature["properties"]["STATE"] != "72":
-    #                 non_list.append((feature["properties"]["STATE"], feature["properties"]["NAME"]))
-    #                 non += 1
-
-    # print(f"{updated} counties were updated out of {len(df)} counties in the data file")
-    # print(f"{non} counties were not updated. There are 3,144 counties in the USA (50 states + D.C.)")
-    # print(f"{updated} + 30 (Alaska) + 1 (Shannon, SD) + 1 (D.C.) = {updated + 30 + 1 + 1} out of 3144 US counties.")
-    # print()
-    # print("States we have county data for: ", *sorted(state_set))
-    # print("** Note: The above list should be from 01-56, but missing: \n02 (Alaska, we don't have data), \
-    #       \n03 (Arizona's old code), \n07 (The old Canal Zone), \n11 (D.C.), \
-    #       \n14 (No state has ever received this code), \n43 (Originally reserved for Puerto Rico), \
-    #       \nand 52 (Again, no state has ever received 52).")
-    
-
-    
-    # print()
-    # print("Counties we don't have data for: ")
-    # print(*non_list)
-
-    # print()
-    # duplicates = df[df.duplicated(subset=["County", "State"], keep=False)]
-    # print("Duplicates in our data file:", len(duplicates))
-    # print(f"{len(df)} rows of data. {len(df)} - 3144 - {len(duplicates)} = {len(df) - len(duplicates)}")
-    # print(f"Not sure where the extra {len(df) - len(duplicates) - 3144} rows are coming from.")
     
     return
 
 
-def updateAlgaeBloomDataCounty(countyJsonFile, countyAlgaeBloomCsv):
-
-
-    return
-
-
-def updateDACDataCounty(countyJsonFile, countyDACCsv):
+def updateDACDataCounty(countyJsonFile):
     # file path variables
     new_file = "backend/geoJsonOutputs/countyData/countyDAC.json"
     data_file = "backend/DAC/dac_class.csv"
@@ -384,55 +324,19 @@ def updateDACDataCounty(countyJsonFile, countyDACCsv):
     # Dump the updated json object into a new file
     with open(new_file, "w") as file:
         json.dump(file_data, file)
-
-    # Quick test
-    with open(new_file, "r") as file:
-        file_data = json.load(file)
-        updated = 0
-        non = 0
-        state_set = set()
-        non_list = list()
-        for feature in file_data["features"]:
-            if "cost" in feature['properties']:
-                updated += 1
-                state_set.add(feature["properties"]["STATE"])
-            else:
-                if feature["properties"]["STATE"] != "72":
-                    non_list.append((feature["properties"]["STATE"], feature["properties"]["NAME"]))
-                    non += 1
-
-    print(f"{updated} counties were updated out of {len(df)} counties in the data file")
-    print(f"{non} counties were not updated. There are 3,144 counties in the USA (50 states + D.C.)")
-    print(f"{updated} + 30 (Alaska) + 1 (Shannon, SD) + 1 (D.C.) = {updated + 30 + 1 + 1} out of 3144 US counties.")
-    print()
-    print("States we have county data for: ", *sorted(state_set))
-    print("** Note: The above list should be from 01-56, but missing: \n02 (Alaska, we don't have data), \
-          \n03 (Arizona's old code), \n07 (The old Canal Zone), \n11 (D.C.), \
-          \n14 (No state has ever received this code), \n43 (Originally reserved for Puerto Rico), \
-          \nand 52 (Again, no state has ever received 52).")
-    
-
-    
-    print()
-    print("Counties we don't have data for: ")
-    print(*non_list)
-
-    print()
-    duplicates = df[df.duplicated(subset=["County", "State"], keep=False)]
-    print("Duplicates in our data file:", len(duplicates))
-    print(f"{len(df)} rows of data. {len(df)} - 3144 - {len(duplicates)} = {len(df) - len(duplicates)}")
-    print(f"Not sure where the extra {len(df) - len(duplicates) - 3144} rows are coming from.")
     
     return
 
 
 def main():
-    updateReforestationDataState("backend/states.json", "dataOutputCSVs/stateData/stateReforestationCsv.csv")
-    # updateAlgaeBloomDataState("backend/states.json", "dataOutputCSVs/stateData/stateAlgaeBloomCsv.csv")
-    # updateDACDataState("backend/states.json", "dataOutputCSVs/stateData/stateDACCsv.csv")
-    updateReforestationDataCounty("backend/counties.json", "dataOutputCSVs/countyData/countyReforestationCsv.csv")
-    # updateAlgaeBloomDataCounty("backend/counties.json", "dataOutputCSVs/countyData/countyAlgaeBloomCsv.csv")
-    # updateDACDataCounty("backend/counties.json", "dataOutputCSVs/countyData/countyDAC.csv")
+    # updateReforestationDataState("backend/states.json")
+    # updateReforestationDataCounty("backend/counties.json")
+
+    # updateDACDataCounty("backend/counties.json")
+    # updateDACDataState("backend/states.json")
+
+    updateKelpFarmData()
+
     print("Done")
 
 
